f2daf7195d4547b320db398b2ef3c7d53efd50b2
Balamurali M <balamurali@frappe.io>
2023-07-17 08:23:12 +0530

    Add more spider maps

--- .config/nvim/lua/spider_config.lua
+++ .config/nvim/lua/spider_config.lua
@@ -1,8 +1,16 @@
--- Keymaps
+-- Motion Keymaps
 vim.keymap.set({"n", "o", "x"}, "w", function() require("spider").motion("w") end, { desc = "Spider-w" })
 vim.keymap.set({"n", "o", "x"}, "e", function() require("spider").motion("e") end, { desc = "Spider-e" })
 vim.keymap.set({"n", "o", "x"}, "b", function() require("spider").motion("b") end, { desc = "Spider-b" })
 vim.keymap.set({"n", "o", "x"}, "ge", function() require("spider").motion("ge") end, { desc = "Spider-ge" })
+
+---	Operator-pending maps
+vim.keymap.set({"n"}, "dw", "de", {remap = true, desc = "Spider-dw"})
+vim.keymap.set({"n"}, "cw", "ce", {remap = true, desc = "Spider-cw"})
+
+-- Insert mode maps
+vim.keymap.set("i", "<C-w>", "<Esc>diwi", { remap = true })
+
 
 -- default values
 require("spider").setup({
